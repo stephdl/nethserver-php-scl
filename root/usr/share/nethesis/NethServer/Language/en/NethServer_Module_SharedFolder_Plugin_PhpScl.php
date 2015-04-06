99c6edd2f552f3374603eb231877bfc7aa217fad
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-04-06 23:51:55 +0200

    * Wed Apr 22 2015 Stephane de Labrusse <stephdl@de-labrusse.fr> 1.0.0-1-ns6 - Initial release to ns6

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_SharedFolder_Plugin_PhpScl.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_SharedFolder_Plugin_PhpScl.php
@@ -4,7 +4,7 @@ $L['PhpScl_Title'] = 'Php Software collection';
 $L['Status_label'] = 'Enable web access';
 $L['PhpVersion_label'] = 'Select the php Version for this Ibay';
 $L['PhpVersion_default_label'] = 'Default Php version';
-$L['PhpVersion_Php54_label'] = 'Php 54 version';
-$L['PhpVersion_Php55_label'] = 'Php 55 version';
-$L['PhpVersion_Php56_label'] = 'Php 56 version';
+$L['PhpVersion_php54_label'] = 'Php 54 version';
+$L['PhpVersion_php55_label'] = 'Php 55 version';
+$L['PhpVersion_php56_label'] = 'Php 56 version';
 
