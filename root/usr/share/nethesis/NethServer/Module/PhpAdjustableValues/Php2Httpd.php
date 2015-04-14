38122e98925ddbbd8a7b9b839001acaf04472cd0
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-04-14 10:57:35 +0200

    php2httpd

---
+++ root/usr/share/nethesis/NethServer/Module/PhpAdjustableValues/Php2Httpd.php
@@ -0,0 +1,28 @@
+<?php
+namespace NethServer\Module\PhpAdjustableValues;
+
+use Nethgui\System\PlatformInterface as Validate;
+
+/**
+ * Implement gui module for php settings
+ */
+
+class Php2Httpd extends \Nethgui\Controller\AbstractController
+{
+
+    public function initialize()
+    {
+
+    $this->declareParameter('PhpVersion', $this->createValidator()->memberOf('php54','php55','php56','default'), array('configuration', 'httpd', 'PhpVersion'));
+
+        parent::initialize();
+    }
+
+
+    public function onParametersSaved($changes)
+    {
+        $this->getPlatform()->signalEvent('nethserver-php-save@post-process');
+    }
+
+}
+
