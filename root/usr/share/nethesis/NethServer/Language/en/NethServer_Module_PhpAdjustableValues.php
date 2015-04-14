11f2554c0b83f585d536dccc89288b246c362175
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-04-14 10:13:57 +0200

    locales

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_PhpAdjustableValues.php
@@ -0,0 +1,28 @@
+<?php
+$L['Php54_Title'] = 'Php V5.4 SCL';
+$L['Php55_Title'] = 'Php V5.5 SCL';
+$L['Php56_Title'] = 'Php V5.6 SCL';
+$L['PhpDefault_Title'] = 'Default Php Version';
+$L['Php54AdjustableValues_Title'] = 'Manage the Php54 Settings';
+$L['Php55AdjustableValues_Title'] = 'Manage the Php55 Settings';
+$L['Php56AdjustableValues_Title'] = 'Manage the Php56 Settings';
+$L['PhpDefaultAdjustableValues_Title'] = 'Manage the default Php Settings';
+
+$L['AllowUrlFopen_label'] = 'Allow php access to remote files';
+$L['Php memory limit (${0})'] = 'Maximum of available memory (${0})';
+$L['Maximum upload file size (${0})'] = 'Maximum upload file size (${0})';
+$L['Maximum post size (${0})'] = 'Maximum size of post data allowed (${0})';
+$L['Maximum execution time (${0})'] = 'Maximum execution time of scripts (${0})';
+$L['Maximum number of files (${0})'] = 'Maximum number of uploaded files (${0})';
+$L['Maximum input time (${0})'] = 'Maximum time to parse input data (${0})';
+
+$L['MemoryLimit_label'] = 'Memory Limit Warning';
+$L['MemoryLimitLabelMustBeSuperiorThanPostMaxSize'] = 'The Maximum of available memory must be superior than the Maximum size of post data allowed';
+$L['PostMaxSize_label'] = 'Post Maximum Limit Warning';
+$L['PostMaxSizeMustBeSuperiorThanUpMaxFileSize'] = 'The Maximum size of post data allowed must be superior than the Maximum upload file size';
+$L['default'] = 'default';
+$L['${0} MB'] = '${0} Megabytes';
+$L['${0} seconds'] = '${0} seconds';
+
+
+
