d3038c4b4eae28798be02a0ea85824d8bb78242b
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-04-14 09:30:17 +0200

    added panel to set php54 php55 php56 and phpdefault

---
+++ root/usr/share/nethesis/NethServer/Template/PhpAdjustableValues/PhpDefault.php
@@ -0,0 +1,21 @@
+<?php
+//echo $view->header()->setAttribute('template', $T('PhpAdjustableValues_Title'));
+
+echo $view->panel()
+
+->insert($view->checkbox('AllowUrlFopen', '1')->setAttribute('uncheckedValue', '0'))
+
+->insert($view->slider('MemoryLimit', $view::SLIDER_ENUMERATIVE | $view::LABEL_ABOVE)
+   ->setAttribute('label', $T('Php memory limit (${0})')))
+
+->insert($view->slider('UploadMaxFilesize', $view::SLIDER_ENUMERATIVE | $view::LABEL_ABOVE)
+    ->setAttribute('label', $T('Maximum upload file size (${0})')))
+
+->insert($view->slider('PostMaxSize', $view::SLIDER_ENUMERATIVE | $view::LABEL_ABOVE)
+    ->setAttribute('label', $T('Maximum post size (${0})')))
+
+->insert($view->slider('MaxExecutionTime', $view::SLIDER_ENUMERATIVE | $view::LABEL_ABOVE)
+    ->setAttribute('label', $T('Maximum execution time (${0})')))
+;
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
+?>
